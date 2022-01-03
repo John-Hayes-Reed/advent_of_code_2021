5f564ab3c2cef17e8ae86343c96d4ed9e12639a5
john hayes-reed <john.hayes.reed@gmail.com>
2022-01-03 16:41:27 +0000

    Change from next to break to reduce redundant loops at end

--- challenge_1_number_of_increases/part_2/depth_measurement_increase_counter.rb
+++ challenge_1_number_of_increases/part_2/depth_measurement_increase_counter.rb
@@ -4,7 +4,7 @@ prev_num = nil
 measurements = File.readlines('input')
 
 total_increases = measurements.each_with_index.reduce(0) do |increases, (measurement, index)|
-  next if (group_measurements = [measurement.to_i, measurements[index + 1].to_i, measurements[index + 2].to_i]).any?(&:nil?)
+  break if (group_measurements = [measurement.to_i, measurements[index + 1].to_i, measurements[index + 2].to_i]).any?(&:nil?)
   group_sum = group_measurements.sum
 
   increase_count = group_sum > (prev_num || Float::INFINITY) ? increases + 1 : increases
